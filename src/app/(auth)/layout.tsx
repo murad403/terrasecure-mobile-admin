573d13f77e16201e7ad65c6dfb2f9c44032d7c73
murad403 <mdmurad.dev2004@gmail.com>
2026-06-16 07:28:10 +0600

    feat: initialize global styles and implement auth route layout with banner component

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,15 @@
+import AuthBanner from '@/components/shared/AuthBanner'
 import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>
-            {children}
+        <div className='min-h-screen grid grid-cols-2 justify-center items-center gap-4'>
+            <div>
+                <AuthBanner />
+            </div>
+            <div className='flex justify-center items-center'>
+                {children}
+            </div>
         </div>
     )
 }
---
+++ src/components/shared/AuthBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthBanner = () => {
+    return (
+        <div className='bg-linear-to-tl from-[#1A2332] to-[#215C9E] md:flex justify-center items-center hidden'>
+            AuthBanner
+        </div>
+    )
+}
+
+export default AuthBanner
