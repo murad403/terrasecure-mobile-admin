33f7d601ebceb32c8daa04e886ecb00969e59393
murad403 <mdm78774@gmail.com>
2026-08-12 08:27:14 +0600

    feat: implement authentication module with sign-in, forgot-password pages, and global layout configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import {Sora } from "next/font/google";
+import { Sora } from "next/font/google";
 import "./globals.css";
 import ReduxWrapper from "@/components/wrapper/ReduxWrapper";
+import { Toaster } from "sonner";
 
 const sora = Sora({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
   subsets: ["latin"],
   variable: "--font-sora"
-})
+});
 
 export const metadata: Metadata = {
   title: "Terrasecure Mobile Admin",
@@ -25,7 +26,10 @@ export default function RootLayout({
       className={`${sora.className} h-full antialiased`}
     >
       <ReduxWrapper>
-        <body className="min-h-full flex flex-col">{children}</body>
+        <body className="min-h-full flex flex-col">
+          {children}
+          <Toaster position="top-right" richColors closeButton />
+        </body>
       </ReduxWrapper>
     </html>
   );
